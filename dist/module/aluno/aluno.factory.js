82a748a9f38bfbe888afe4da752e2e2d16b68763
Your Name <raphael.martorella@hotmail.com>
2023-09-25 12:31:25 -0300

    Remove 'dist' folder from Git tracking

--- dist/module/aluno/aluno.factory.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const aluno_model_1 = require("./aluno.model");
-const knex_1 = require("../../service/knex");
-exports.default = new aluno_model_1.AlunoClass(new knex_1.knexService);
